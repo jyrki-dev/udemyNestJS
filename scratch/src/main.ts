b32d15312bc2fd447943e7a3c379857c9843ef25
Jyrki Kokkola <j.j.kokkola@gmail.com>
2022-10-26 22:16:29 +0300

    2.7

---
+++ scratch/src/main.ts
@@ -0,0 +1,14 @@
+import { Controller, Module, Get } from '@nestjs/common';
+
+@Controller()
+class AppController {
+  @Get()
+  getRootRoute() {
+    return 'hi there!';
+  }
+}
+
+@Module({
+  controllers: [AppController],
+})
+class AppModule {}
