f70d9d79ba4da1ffa5047f8ad9a8b0bd4af82f9c
Jyrki Kokkola <j.j.kokkola@gmail.com>
2022-10-27 01:19:57 +0300

    23

---
+++ messages/src/messages/messages.repository.ts
@@ -0,0 +1,20 @@
+import { readFile, writeFile } from 'fs/promises';
+
+export class MessagesRepository {
+  async findOne(id: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+    return messages[id];
+  }
+
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+    return messages;
+  }
+
+  async create(message: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+  }
+}
